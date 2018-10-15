69e939e4dbb0ab8d7da5ec35474af298fd6d8e6b
Juan Francisco <jfmarquezalcaide@gmail.com>
2018-10-15 14:04:23 +0200

    Añadida pagina inicial, pagina nosotros, bares y tapas

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,45 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Tapa;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function homepageAction(Request $request)
+    {
+        $tapaRepository = $this->getDoctrine()->getRepository(Tapa::class);
+        $tapas = $tapaRepository->findByTop(1);
+
+        return $this->render('frontal/index.html.twig', array('tapas'=>$tapas));
+    }
+
+    /**
+     * @Route("/nosotros", name="nosotros")
+     */
+    public function nosotrosAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('frontal/nosotros.html.twig');
+    }
+
+    /**
+     * @Route("/contactar/{sitio}", name="contactar")
+     */
+    public function contactarAction(Request $request, $sitio = "todos")
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/bares.html.twig', array("sitio" => $sitio));
+    }
+
+    /**
+     * @Route("/tapa/{id}", name="tapa")
+     */
+    public function tapaAction(Request $request, $id = null)
+    {
+        // replace this example code with whatever you need
+        return $this->render('frontal/tapa.html.twig', array("tapa" => $id));
     }
 }
